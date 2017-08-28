40e1dc159f0ab55fb75588b58c4caf51b4cc71a4
Erick Bernardo <erickvictor@gmail.com>
2017-08-27 22:11:10 -0400

    Update Summary

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -16,7 +16,7 @@
       $http.get(`${url}?skip=${(page - 1) * 10}&limit=10`).then(function(response) {
         vm.billingCycle = {credits: [{}], debts: [{}]}
         vm.billingCycles = response.data
-      //  vm.calculateValues()
+        vm.calculateValues()
 
         $http.get(`${url}/count`).then(function(response) {
           vm.pages = Math.ceil(response.value / 10)
@@ -36,13 +36,13 @@
 
     vm.showTabUpdate = function(billingCycle) {
       vm.billingCycle = billingCycle
-//      vm.calculateValues()
+      vm.calculateValues()
       tabs.show(vm, {tabUpdate: true})
     }
 
     vm.showTabDelete = function(billingCycle) {
       vm.billingCycle = billingCycle
-//      vm.calculateValues()
+      vm.calculateValues()
       tabs.show(vm, {tabDelete: true})
     }
 
@@ -72,11 +72,13 @@
 
     vm.cloneCredit = function(index, {name, value}) {
       vm.billingCycle.credits.splice(index + 1, 0, {name, value})
+      vm.calculateValues()
     }
 
     vm.deleteCredit = function(index) {
       if(vm.billingCycle.credits.length > 1) {
         vm.billingCycle.credits.splice(index, 1)
+        vm.calculateValues()
       }
     }
 
@@ -86,12 +88,31 @@
 
     vm.cloneDebt = function(index, {name, value, status}) {
       vm.billingCycle.debts.splice(index + 1, 0, {name, value, status})
+      vm.calculateValues()
     }
 
     vm.deleteDebt = function(index) {
       if(vm.billingCycle.debts.length > 1) {
         vm.billingCycle.debts.splice(index, 1)
+        vm.calculateValues()
       }
+    }
+
+    vm.calculateValues = function() {
+      vm.credit = 0
+      vm.debt = 0
+
+      if(vm.billingCycle) {
+        vm.billingCycle.credits.forEach(function({value}){
+          vm.credit += !value || isNaN(value) ? 0 : parseFloat(value)
+        })
+
+        vm.billingCycle.debts.forEach(function({value}) {
+          vm.debt += !value || isNaN(value) ? 0 : parseFloat(value)
+        })
+      }
+
+      vm.total = vm.credit -vm.debt
     }
 
     vm.refresh()
